60f00e42e6a0f43d6ddb9795b9dac117d8d78ac2
Tenzin <tenzins97@gmail.com>
2023-04-11 16:58:25 -0400

    switching from Celsius to Fahrenheit

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import { useState } from 'react';
 
 
 function App() {
+
   const [currentWeather, setCurrentWeather] = useState(null);
   const [forecast, setForecast] = useState(null);
 
   const handleOnSearchChange = (searchData) => {
+
     const [lat, lon] = searchData.value.split(" ")
 
     const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=imperial`
@@ -21,24 +23,29 @@ function App() {
 
     Promise.all([currentWeatherFetch, forecastFetch])
       .then(async (response) => {
+
         const weatherResponse = await response[0].json();
         const forecastResponse = await response[1].json();
 
         setCurrentWeather({ city: searchData.label, ...weatherResponse }); //label coming from the search.js, code of country
         setForecast({ city: searchData.label, ...forecastResponse });
       })
+
       .catch((err) => console.log(err));
+
   }
 
   console.log(currentWeather);
   console.log(forecast); //this will provide the data in which I will then have it displayed in the web page instead of console.
 
   return (
+
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather} />}
       {forecast && <Forecast data={forecast} />}
     </div>
+
   );
 }
 
